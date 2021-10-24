09b942f7ab2359334019c9123c85df5dbdf10f33
Harshita Bambure <harshitabambure@gmail.com>
2021-10-24 17:16:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/Data.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerview
+
+data class Data(val name:String,val email:String)
---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+ private lateinit var binding: ActivityMainBinding
+ private lateinit var adapter: UserAdapter
+  private lateinit var reyclerview : RecyclerView
+  private lateinit var layoutManager: LinearLayoutManager
+  private lateinit var user :List<Data>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        userList()
+       binding.rvRecycler.layoutManager = LinearLayoutManager(this)
+       adapter = UserAdapter(user)
+        binding.rvRecycler.adapter = adapter
+
+    }
+    private fun userList(){
+        user = listOf(
+            Data("Harshita","Harshita@gmail.com"),
+            Data("minu","minu@gmail.com"),
+            Data("ram","ram@gmail.com"),
+            Data("sham","sham@gmail.com"),
+            Data("raja","raja@gmail.com"),
+            Data("harsh","harsh@gmail.com"),
+            Data("harshu","harshu@gmail.com"),
+            Data("xyz","xyz@gmail.com"),
+            Data("abc","abc@gmail.com"),
+            Data("pqr","pqr@gmail.com")
+        )
+    }
+
+}
